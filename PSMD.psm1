9a08fdc177bc6570483ab2db0bfc8f1a4a6ba00c
TAABAKE4 <kevin.bates@swisscom.com>
2018-07-11 14:48:18 +0200

    minor change for the classes

--- PSMD.psm1
+++ PSMD.psm1
@@ -1,4 +1,18 @@
 $ScriptPath = Split-Path $MyInvocation.MyCommand.Path
+
+write-verbose "Loading Private Classes"
+$PrivateClasses = gci "$ScriptPath\Classes\private" -Filter *.ps1 | Select -Expand FullName
+
+
+foreach ($private in $PrivateClasses){
+    write-verbose "importing Class $($private)"
+    try{
+        . $public
+    }catch{
+        write-warning $_
+    }
+}
+
 <#
 write-verbose "Loading Private Functions"
 $PrivateFunctions = gci "$ScriptPath\Functions\Private" -Filter *.ps1 | Select -Expand FullName
@@ -20,19 +34,6 @@ $PublicFunctions = gci "$ScriptPath\Functions\public" -Filter *.ps1 | Select -Ex
 
 foreach ($public in $PublicFunctions){
     write-verbose "importing function $($public)"
-    try{
-        . $public
-    }catch{
-        write-warning $_
-    }
-}
-
-write-verbose "Loading Private Classes"
-$PrivateClasses = gci "$ScriptPath\Classes\private" -Filter *.ps1 | Select -Expand FullName
-
-
-foreach ($private in $PrivateClasses){
-    write-verbose "importing Class $($private)"
     try{
         . $public
     }catch{
